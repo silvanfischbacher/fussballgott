ced3eda61bcf0f581143949b2d7ec9a7dffacd79
Silvan Fischbacher <silvan.fischbacher@greenmail.ch>
2021-06-07 16:44:38 +0200

    more plotting options

--- fussballgott/plot.py
+++ fussballgott/plot.py
@@ -8,15 +8,27 @@ def league(sim):
     ax.xaxis.set_label_position('top')
     ax.tick_params(length=0)
 
-def tournament(sim):
+def tournament(sim, sort_for = None, save = None, prob_style = 'standard'):
+    if prob_style=='standard':
+        sim = sim
+    elif prob_style=='cumulative':
+        sim = sim.iloc[:, ::-1].cumsum(axis=1).iloc[:, ::-1]
+    else:
+        print('prob_style {} is not implemented, use "standard" or "cumulative"'.format(prob_style))
+
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green"])
     plt.figure(figsize=(11,10))
-    ax = sns.heatmap(sim,cmap=cmap,vmin=0,vmax=1,annot=True,fmt='.2')
+    if sort_for is None:
+        ax = sns.heatmap(sim, cmap=cmap, vmin=0, vmax=1, annot=True, fmt='.2')
+    else:
+        ax = sns.heatmap(sim.sort_values(by=sort_for, ascending = False), cmap=cmap, vmin=0, vmax=1, annot=True, fmt='.2')
     ax.xaxis.tick_top()
     ax.xaxis.set_label_position('top')
     ax.tick_params(length=0)
+    if save is not None:
+        plt.savefig(save)
 
-def game_stat(sim, winprob, team1='home', team2='away'):
+def game_stat(sim, winprob=None, team1='home', team2='away'):
     cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white","green"])
     plt.figure(figsize=(11,10))
     ax = sns.heatmap(sim,cmap=cmap,annot=True,fmt='.3',)
@@ -26,5 +38,6 @@ def game_stat(sim, winprob, team1='home', team2='away'):
     plt.ylabel(team1)
     ax.tick_params(length=0)
 
-    plt.figure()
-    plt.bar(x=['1','X','2'],height=winprob)
+    if winprob is not None:
+        plt.figure()
+        plt.bar(x=['1','X','2'],height=winprob)
